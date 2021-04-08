e89f09bf3a777ce158ab2836f4a39046f3a11797
bhavesh <bkpassi2017@gmail.com>
2021-04-08 23:34:13 +0530

    Added Home and Job Detail Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import {Redirect, Route, Switch} from "react-router-dom";
-
 import Login from "./components/login/login";
 import SignUpMain from "./components/signup/signupMain";
 import Header from "./components/header_footer.js/header";
 import Footer from "./components/header_footer.js/footer";
 import UserProfile from "./components/userProfile/userProfile";
+import Home from './components/home/home';
+import JobDetail from './components/job/job';
 
 function App() {
   return (
     <div className="App">
        <Header />
-      <Switch>
-        <Route path = "/login" exact component={Login}/>
-        <Route path = "/signup" exact component={SignUpMain}/>
-        <Route path = "/user" exact component={UserProfile}/>
-        <Redirect to="/"/>
-      </Switch>
+          <Switch>
+            <Route path = "/login" exact component={Login}/>
+            <Route path = "/signup" exact component={SignUpMain}/>
+            <Route path = "/user" exact component={UserProfile}/>
+            <Redirect to="/"/>
+          </Switch>
       <Footer /> 
     </div>
   );
---
+++ src/components/home/home.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+import { Card, Button, FormGroup, Label, Input,
+    CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardSubtitle, CardText, CardImg} from 'reactstrap';
+import Header from '../header_footer.js/header';
+import Footer from '../header_footer.js/footer';
+import SDE from '../../images/sde.jpg'
+import './home.css'
+
+const data = [
+    {id: 0, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+    {id: 1, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+    {id: 2, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+    {id: 3, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+    {id: 4, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+    {id: 5, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+    {id: 6, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+    {id: 7, company_name: "Microsoft", Profile: "Software Developer", category: "Full Time", CTC: "150k"},
+
+]
+
+class Home extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    RenderMenuItem = (job)=>{
+
+        return ( 
+    
+            <Card className='job'>
+            <CardBody>
+                <CardTitle tag="h6" className='job-heading'>{job.category} Hire</CardTitle>
+            </CardBody>
+                <CardImg className='job-img' src={SDE}/>
+            <CardBody>
+                <CardSubtitle tag="h6" className="mb-2 mt-2 text-muted">
+                    <span className='fa fa-building fa-lg mr-3 company-icon'/>    
+                    {job.company_name}
+                </CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    <span className='fa fa-id-card mt-3 mr-2 fa-lg profile-icon'/>    
+                    {job.Profile}
+                </CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    <span className='fa fa-money mr-3 mt-3 fa-lg money-icon'/>    
+                    {job.CTC} CTC
+                </CardSubtitle>
+                <CardSubtitle tag="h6" style={{color: '#1597bb'}} className="mb-2 mt-3">
+                    10-04-2021 Coding Round
+                </CardSubtitle>
+                <div className='row mt-4'>
+                    <Button className='col-12 mt-3' color='danger'><span className='fa fa-lg fa-info-circle mr-2 ml-2' />View Details</Button>
+                </div>
+            </CardBody>
+          </Card>
+        );
+    }
+
+    renderCardList = ()=>{
+        return data.map((job) => {  
+            return (
+                <div className="col-12 col-lg-3 col-md-6 col-sm-6 mt-1 mb-4"  key={job.id}>
+                    {this.RenderMenuItem(job)}
+                </div>
+            );
+        });
+    }
+
+    render(){
+        return(
+            <div>
+            <Header/>
+                <div className="container jobs">
+                    <div className='row justify-content-center mt-3'>
+                        <h2 style={{textAlign: 'center', textShadow: '0 0 3px #FF0000'}}>ALL JOB OPENINGS</h2>
+                    </div>
+                    <div className="row  justify-content-center mt-4" >
+                        {this.renderCardList()}
+                    </div>
+                </div>
+            <Footer/>
+            </div>
+        );
+    }
+}
+
+export default Home;
